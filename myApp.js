637a03ef31384231578fa562ce40e7128643be67
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-08-22 20:35:50 -0400

    updated Hello World to Hello Express

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 
 app.get("/", (req, res) => {
-    console.log("Hello World");
-    res.send("Hello World");
+    console.log("Hello Express");
+    res.send("Hello Express");
 });
 
 
