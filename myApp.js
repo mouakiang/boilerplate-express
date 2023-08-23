9c9c6fe2085e2ecfdc37eedfd540a3f9a97c2959
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-08-23 10:52:57 -0400

    changed get call to send file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => {
-    console.log("Hello Express");
-    res.send("Hello Express");
-});
+// app.get("/", (req, res) => {
+//     console.log("Hello Express");
+//     res.send("Hello Express");
+// });
 
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
 
