3c89f86b1ff2e83bda979db21e5a89d8a564f249
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-08-23 21:14:38 -0400

    add quotations

--- myApp.js
+++ myApp.js
@@ -12,8 +12,8 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
     res.json({
-        message: "Hello Json"
-    });
+        "message": "Hello json"
+        });
 });
 
 
