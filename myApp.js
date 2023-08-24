16214fcf69a90c296ed2ff74576dc305957a4c9f
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-08-23 21:11:17 -0400

    routed json

--- myApp.js
+++ myApp.js
@@ -6,11 +6,15 @@ let app = express();
 //     res.send("Hello Express");
 // });
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+// app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
+app.get("/json", (req, res) => {
+    res.json({
+        message: "Hello Json"
+    });
+});
 
 
 
